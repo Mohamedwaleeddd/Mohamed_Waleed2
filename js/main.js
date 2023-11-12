7bde5f74dce419f53b129ea3be5fb968e5764e83
MW_Dev <122593986+Mohamedwaleeddd@users.noreply.github.com>
2023-11-12 22:34:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+const contact = document.getElementById("contact");
 const contactForm = document.querySelector(".contact form");
 
 contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const messageText = document.getElementById("message-text").value;
-  const messageCode = messageText.split(" ").join("%20");
-  console.log(messageCode);
-  location.assign(`https://wa.me/+201556135667?text=${messageCode}`);
+  const messageCode = encodeURIComponent(messageText);
+  window.open(`https://wa.me/+201556135667?text=${messageCode}`, "_blank");
 });
